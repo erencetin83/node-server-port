6d4ea74e1f8f8e3c8d197de121ecd00d067137f4
erencetin83 <eren.cetin@gmail.com>
2017-06-29 03:16:17 +0300

    Initial commit

---
+++ apps/FirstApp/Mdl_test.js
@@ -0,0 +1,19 @@
+//Sample
+
+
+module.exports = {
+
+  foo : function(){
+
+
+    console.log("test0 foo");
+
+  },
+  bar : function(){
+
+    console.log("test0 bar");
+
+  }
+
+}
+ 
---
+++ apps/SecondApp/test2.js
@@ -0,0 +1,19 @@
+
+
+
+module.exports = {
+
+  foo : function(){
+
+
+    console.log("test2 foo");
+
+  },
+  bar : function(){
+
+    console.log("test2 bar");
+
+  }
+
+}
+ 
